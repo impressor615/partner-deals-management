62d3acc5c676a92b96816bcf22b60f27d772f8d2
seonghyeon <hyeon@apartmentary.com>
2018-11-29 00:23:09 +0900

    add bootstrap, reactstrap

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
-const Hello = () => <div>Hello World</div>;
+const Hello = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Button>Button</Button>
+  </div>
+
+);
 
 export default Hello;
